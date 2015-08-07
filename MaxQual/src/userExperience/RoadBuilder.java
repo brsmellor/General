abacc1f1c8282b496d3f45a2605c9da031bda866
wsiadmin <wsiadmin@SQA-Z820.ad.wsicorp.com>
2015-08-07 14:56:53 -0400

    some new stuff

---
+++ MaxQual/src/userExperience/RoadBuilder.java
@@ -0,0 +1,19 @@
+package userExperience;
+
+import org.sikuli.script.*;
+import java.io.File.*;
+
+public class RoadBuilder{
+	
+	private Region menuBar;
+	private Region buildButtons;
+	private Region roadBuilderTab;
+	private Region earthTab;
+	
+	
+	
+	public RoadBuilder(){
+		
+	}
+
+}
